a6eaa6eea3fae2d240a63cbaf8ec7cdfbf1d1d7c
Yevhen Voiuiev <yevhen.voiuiev@gmail.com>
2020-03-12 09:40:06 +0200

    Making an Interactive Component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('click')}>
         {this.props.value}
       </button>
     );
